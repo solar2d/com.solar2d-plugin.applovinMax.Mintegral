9bf9b45f20556ca117b039384a7bb70c86afaf2d
Scott Harrison <scottrules44@gmail.com>
2024-06-17 16:37:46 -0500

    Max updates 12.5

--- plugins/2018.3326/iphone-sim/metadata.lua
+++ plugins/2018.3326/iphone-sim/metadata.lua
@@ -6,9 +6,9 @@ local metadata =
 
 		-- This is the name without the 'lib' prefix.
 		-- In this case, the static library is called: libSTATIC_LIB_NAME.a
-		staticLibs = {"MTGSDKSplash","MTGSDK", "MTGSDKBanner", "MTGSDKNewInterstitial", "MTGSDKInterstitial","MTGSDKInterstitialVideo", "MTGSDKBidding", "MTGSDKReward", "MTGSDKNativeAdvanced", "MintegralAdapter"},
+		staticLibs = {"MintegralAdapter"},
 
-		frameworks = { },
+		frameworks = {"MTGSDK", "MTGSDKBanner", "MTGSDKBidding", "MTGSDKInterstitialVideo", "MTGSDKNativeAdvanced", "MTGSDKNewInterstitial", "MTGSDKReward", "MTGSDKSplash" },
 		frameworksOptional = {},
 	},
 }
